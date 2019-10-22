f9e703597cbdd4d3ab1955a9f62ad295f1ab88a1
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2019-10-22 16:34:03 +0300

    Update cgi101.py

--- cgi/cgi101.py
+++ cgi/cgi101.py
@@ -1,3 +1,5 @@
+#!C:\Programs\Python\Python37\
+
 import cgi
 import html
 
@@ -5,6 +7,6 @@ form = cgi.FieldStorage()           # парсинг данных формы
 print('Content-type: text/html\n')  # http-заголовок плюс пустая строка
 print('<title>Reply Page</title>')  # html-разметка ответа
 if 'user' not in form:
-    print('<h1>Who are you and WHAT ARE YOU DOING HERE?!!!</h1>')
+    print('<h1>Who are you, and WHAT ARE YOU DOING HERE?!!!</h1>')
 else:
     print('<h1>Hello <i>{}</i></h1>'.format(html.escape(form['user'].value)))

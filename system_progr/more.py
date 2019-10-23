5ce360a1062fc688f7790ad4a4548bc7c0f2ec78
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2019-10-23 16:18:08 +0300

    Create more.py
    New Part of Lutz's book - System programming

---
+++ system_progr/more.py
@@ -0,0 +1,20 @@
+"""
+разбивает строку или текстовый файл на страницы для интерактивного просмотра
+"""
+
+
+def more(text, num_lines=15):
+    lines = text.splitlines()       # подобно split('\n') но без '' в конце
+    print(lines)
+    while lines:
+        chunk = lines[:num_lines]
+        lines = lines[num_lines:]
+        for line in chunk:
+            print(line)
+        if lines and input('More?') not in ['y', 'Y']:
+            break
+
+
+if __name__ == '__main__':                  # если запускается как сценарий
+    import sys                              # отобразить построчное содержимое
+    more(open(sys.argv[1]).read(), 10)      # файла, указанного в командной строке

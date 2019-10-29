491de36ed6086413551bdeed88d4d6bdcaeed7ca
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2019-10-29 15:41:23 +0300

    Updated call_subprocess_universal.py

--- system_progr/call_subprocess_universal.py
+++ system_progr/call_subprocess_universal.py
@@ -15,3 +15,21 @@ X3 = pipe3.stdout.read()
 print(X3)
 X3_return = pipe3.wait()             # код завершения
 print('Close code:', X3_return)
+
+#############################################################
+pipe4 = Popen('python hello_in.py', stdin=PIPE)
+X4 = pipe4.stdin.write(b'Pokey\n')
+print(X4)
+pipe4.stdin.close()
+X4_result = pipe4.wait()
+print(X4_result)
+open('hello_in.txt').read()         # вывод был отправлен в файл
+
+#############################################################
+pipe5 = Popen('python reader.py', stdin=PIPE, stdout=PIPE)
+X5 = pipe5.stdin.write(b'Lumberjack\n')
+print(X5)
+pipe5.stdin.write(b'12\n')
+output = pipe5.stdout.read()
+X5_result = pipe5.wait()
+print('Close code:', X5_result)
